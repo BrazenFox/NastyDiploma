d64b635b0b537f83e2a4a429c0fb6497c47526e7
marokhonovdmitry1998@gmail.com <2157040Kv178>
2022-06-19 18:05:44 +0300

    commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
 import matplotlib.pyplot as plt
 import numpy as np
+import plotly.graph_objects as go
 
 C = 0.12
 NI = 36.0
@@ -86,23 +87,39 @@ def rasschet_Xm_Ym_by_Z_Tmax(Z, T):
 
 
 fig, ax = plt.subplots()
-
+Z=0
 count_of_graph = 5
-T = rasschet_Tmax(0, TL)
+T = rasschet_Tmax(Z, TL)
 XM = [0] * count_of_graph
 YM = [0] * count_of_graph
 TT = [((i+1) * T/count_of_graph) for i in range(count_of_graph)]
 x = np.arange(-5, 1, 0.1)
 t_array = [[0] * len(x) for i in range(count_of_graph)]
 for t in range(count_of_graph):
-    xm, ym, tm, tt = rasschet_Xm_Ym_by_Z_Tmax(0, TT[t])
+    xm, ym, tm, tt = rasschet_Xm_Ym_by_Z_Tmax(Z, TT[t])
     XM[t] = xm
     YM[t] = ym
     print(YM[t])
     for i in range(len(x)):
-        t_array[t][i] = rasschet_T_by_X_Y_Z(x[i], YM[t], 0)
-    ax.plot(x, t_array[t])
-ax.plot(XM, TT)
+        t_array[t][i] = rasschet_T_by_X_Y_Z(x[i], YM[t], Z)
+    ax.plot(x, t_array[t], label = u'Y '+str(t))
+
+"""XM_plot = [0] * 100
+YM_plot = [0] * 100
+TT_plot = [((i+1) * T/100) for i in range(100)]
+for t in range(100):
+    xm, ym, tm, tt = rasschet_Xm_Ym_by_Z_Tmax(Z, TT_plot[t])
+    XM_plot[t] = xm
+    YM_plot[t] = ym
+ax.plot(XM_plot, TT_plot)"""
+
+
+plt.title(u'График распределения температур по оси X для различных значений Y по схеме ДТ-ПТ')
+plt.xlabel(u'X, см')
+plt.ylabel(u'Температура, T\N{DEGREE SIGN}C')
+ax.plot(XM, TT, 'r--',label = u'Максимумы')
+plt.legend()
+plt.grid(True)
 plt.show()
 
 
@@ -110,13 +127,16 @@ plt.show()
 fig, ax = plt.subplots()
 
 for i in range(count_of_graph):
-    ax.plot(-x/V, t_array[i])
+    ax.plot(-x/V, t_array[i], label = u'Y '+str(t))
 XM = np.array(XM)
-ax.plot(-XM/V, TT)
+ax.plot(-XM/V, TT, 'r--',label = u'Максимумы')
+
+plt.title(u'График термического цикла по схеме ДТ-ПТ')
+plt.xlabel(u't,c')
+plt.ylabel(u'Температура, T\N{DEGREE SIGN}C')
+plt.legend()
+plt.grid(True)
 plt.show()
-
-
-
 
 
 x = np.arange(-10,2,0.01)
@@ -129,7 +149,7 @@ for i in range(len(y)):
 
 fig, ax = plt.subplots()
 
-ax.contour(X,Y,t_array, levels = 10)
+ax.contour(X,Y,t_array, levels = 5)
 
 max_index=[0]*len(t_array)
 for i in range(len(t_array)):
@@ -144,11 +164,38 @@ Ymax=[0]*len(max_index)
 for i in range(len(max_index)):
     Xmax[i] = X[i][max_index[i]]
     Ymax[i] = Y[i][max_index[i]]
-ax.plot(Xmax, Ymax)
-#fig.set_figwidth(12)    #  ширина и
-#fig.set_figheight(12)    #  высота "Figure"
+ax.plot(Xmax, Ymax, 'r--',label = u'Максимумы')
+plt.xlabel(u'X,см')
+plt.ylabel(u'Y,см')
+plt.title("Температурное поле предельного состояния по схеме ДТ-ПТ")
+plt.legend()
+plt.grid(True)
 
 plt.show()
+
+x_count = 200
+y_count = 100
+z_count = 100
+
+X, Y, Z = np.mgrid[-20:2:200j, -5:5:100j, -5:5:100j]
+values=[[[0 for i in range(z_count)] for j in range(y_count)] for k in range(x_count)]
+for i in range(x_count):
+    for j in range(y_count):
+        for k in range(z_count):
+            values[i][j][k]=rasschet_T_by_X_Y_Z(X[i][j][k], Y[i][j][k], Z[i][j][k])
+
+values = np.array(values)
+fig = go.Figure(data=go.Volume(
+    x=X.flatten(),
+    y=Y.flatten(),
+    z=Z.flatten(),
+    value=values.flatten(),
+    isomin=0,
+    isomax=1500,
+    opacity=0.1, # needs to be small to see through all surfaces
+    surface_count=21, # needs to be a large number for good volume rendering
+    ))
+fig.show()
 
 
 
